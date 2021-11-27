07423c5065e13876264046168012c4852c2b2a59
beerappa <beerappadandaragi@gmail.com>
2021-11-27 21:59:05 +0530

    Possword

--- UserRegistration/UserRegistration/Patterns.cs
+++ UserRegistration/UserRegistration/Patterns.cs
@@ -10,20 +10,20 @@ namespace UserRegistration
     class Patterns
     {
 
-        
-            public static void Password(string name)
+
+        public static void Password(string name)
+        {
+            string Password = "^[A-Z][A-Za-z0-9]{7,}";
+            Regex regex = new Regex(Password);
+            if (regex.IsMatch(name))
             {
-                string Password = "^[A-Za-z0-9]{8,}";
-                Regex regex = new Regex(Password);
-                if (regex.IsMatch(name))
-                {
-                    Console.WriteLine(name + " is valid Possword");
-                }
-                else
-                {
-                    Console.WriteLine(name + " is invalid possword.");
-                }
-            Console.ReadLine();
+                Console.WriteLine(name + " is valid possword");
             }
+            else
+            {
+                Console.WriteLine(name + " is invalid possword");
+            }
+            Console.ReadKey();
         }
     }
+}
