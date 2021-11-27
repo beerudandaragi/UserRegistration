e61f389c6d9d3eab4695adbd0a6470befc3d162c
beerappa <beerappadandaragi@gmail.com>
2021-11-27 22:21:07 +0530

    Combine All

--- UserRegistration/UserRegistration/Patterns.cs
+++ UserRegistration/UserRegistration/Patterns.cs
@@ -1,27 +1,79 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
-namespace UserRegistration
+namespace RegEx
 {
-    class Patterns
+    public class Patterns
     {
-
-
-        public static void Password(string name)
+        public static void FirstName(string name1)
         {
-            string Password = "^.*(?=.{8,})(?=.*)(?=.*[a-z])(?=.*[A-Z])(?=.*[!*@#$%^&+=]).*$";
-            Regex regex = new Regex(Password);
-            if (regex.IsMatch(name))
+            string FirstName = "^[A-Za-z]\\w{1,10}$$";
+            Regex regex = new Regex(FirstName);
+            if (regex.IsMatch(name1))
             {
-                Console.WriteLine(name + " is valid possword");
+                Console.WriteLine(name1 + " is valid First name");
             }
             else
             {
-                Console.WriteLine(name + " is invalid possword.");
+                Console.WriteLine(name1 + " is invalid. Please Enter First name start with capital letter and maximum 10 charactors");
+            }
+        }
+
+        public static void LastName(string name2)
+        {
+            string LastName = "^[A-Za-z]\\w{1,10}$$";
+            Regex regex = new Regex(LastName);
+            if (regex.IsMatch(name2))
+            {
+                Console.WriteLine(name2 + " is valid Last Name name");
+            }
+            else
+            {
+                Console.WriteLine(name2 + " is invalid. Please Enter Last name start with capital letter and maximum 10 charactors");
+            }
+        }
+
+        public static void Email(string name3)
+        {
+            string Email = "^[A-Z0-9a-z]{1,}([.#$^][A-Za-z0-9]+)*[@][A-Za-z]{2,}[.][A-Za-z]{2,3}([.][a-zA-Z]{2})?$";
+            Regex regex = new Regex(Email);
+            if (regex.IsMatch(name3))
+            {
+                Console.WriteLine(name3 + " is valid Email");
+            }
+            else
+            {
+                Console.WriteLine(name3 + " is invalid.");
+            }
+        }
+
+        public static void MobileNumber(string name4)
+        {
+            string MobileNumber = "(0|91)?[ ][6-9][0-9]{9}";
+            Regex regex = new Regex(MobileNumber);
+            if (regex.IsMatch(name4))
+            {
+                Console.WriteLine(name4 + " is MObile Number");
+            }
+            else
+            {
+                Console.WriteLine(name4 + " is invalid.");
+            }
+        }
+
+        public static void Password(string name5)
+        {
+            string Password = "^.*(?=.{8,})(?=.*)(?=.*[a-z])(?=.*[A-Z])(?=.*[!*@#$%^&+=]).*$";
+            Regex regex = new Regex(Password);
+            if (regex.IsMatch(name5))
+            {
+                Console.WriteLine(name5 + " is valid possword");
+            }
+            else
+            {
+                Console.WriteLine(name5 + " is invalid.");
             }
             Console.ReadKey();
         }
--- UserRegistration/UserRegistration/Program.cs
+++ UserRegistration/UserRegistration/Program.cs
@@ -1,22 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace UserRegistration
+namespace RegEx
 {
     class Program
     {
         static void Main(string[] args)
-
-        { 
-        
+        {
             Console.WriteLine("Welcome to Regular Expression Program \n");
-            Console.WriteLine("Write your Password ex : Aa9@ length atleast 8 .");
+
+            Console.WriteLine("Write your name using 1st character should be Capital ");
+            Console.Write("\n Enter First Name : ");
+            string name1 = Console.ReadLine();
+            Patterns.FirstName(name1);
+
+            Console.WriteLine("\n Write your name using 1st character should be Capital ");
+            Console.Write("\n Enter Last Name : ");
+            string name2 = Console.ReadLine();
+            Patterns.LastName(name2);
+
+            Console.WriteLine("\n Write your Email ex : beerappadandaragi@gmail.com your can write yahoo and any other mail ID.");
+            Console.Write("\n Enter Email : ");
+            string name3 = Console.ReadLine();
+            Patterns.Email(name3);
+
+            Console.WriteLine("\n Write your Mobile Number ex : +91 7411416743 .");
+            Console.Write("\n Enter Mobile Number : ");
+            string name4 = Console.ReadLine();
+            Patterns.MobileNumber(name4);
+
+            Console.WriteLine("\n Write your Password ex : Aa9@ length atleast 8 .");
             Console.Write("\n Enter Password : ");
-            string name = Console.ReadLine();
-            Patterns.Password(name);
+            string name5 = Console.ReadLine();
+            Patterns.Password(name5);
         }
     }
 }
