0066402a2786f95333ad92ffd710e24c54917b9b
beerappa <beerappadandaragi@gmail.com>
2021-11-27 22:12:04 +0530

    Possword

--- UserRegistration/UserRegistration/Patterns.cs
+++ UserRegistration/UserRegistration/Patterns.cs
@@ -10,19 +10,20 @@ namespace UserRegistration
     class Patterns
     {
 
+
         public static void Password(string name)
         {
-            string Password = "^((?=.*[A-Z])(?=.*[0-9])(?=.{8,}))";
+            string Password = "^.*(?=.{8,})(?=.*)(?=.*[a-z])(?=.*[A-Z])(?=.*[!*@#$%^&+=]).*$";
             Regex regex = new Regex(Password);
             if (regex.IsMatch(name))
             {
-                Console.WriteLine(name + " is possword");
+                Console.WriteLine(name + " is valid possword");
             }
             else
             {
-                Console.WriteLine(name + " is invalid.");
+                Console.WriteLine(name + " is invalid possword.");
             }
-            Console.ReadKey() ;
+            Console.ReadKey();
         }
     }
 }
--- UserRegistration/UserRegistration/Program.cs
+++ UserRegistration/UserRegistration/Program.cs
@@ -9,9 +9,11 @@ namespace UserRegistration
     class Program
     {
         static void Main(string[] args)
-        {
+
+        { 
+        
             Console.WriteLine("Welcome to Regular Expression Program \n");
-            Console.WriteLine("Write your Password ex : Aa9 length atleast 8 .");
+            Console.WriteLine("Write your Password ex : Aa9@ length atleast 8 .");
             Console.Write("\n Enter Password : ");
             string name = Console.ReadLine();
             Patterns.Password(name);
