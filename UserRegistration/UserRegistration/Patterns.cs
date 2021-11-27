0714e1ee3ace4659ec7766de5a452e680c9d5409
beerappa <beerappadandaragi@gmail.com>
2021-11-27 21:26:46 +0530

    FirstName

---
+++ UserRegistration/UserRegistration/Patterns.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    class Patterns
+    {
+        public static void FirstName(string name)
+        {
+            string FirstName = "^[A-Za-z]\\w{1,10}$$";
+            Regex regex = new Regex(FirstName);
+            if (regex.IsMatch(name))
+            {
+                Console.WriteLine(name + " is valid name");
+            }
+            else
+            {
+                Console.WriteLine(name + " is invalid. Please Enter First name start with capital letter and maximum 10 charactors");
+            }
+            Console.ReadKey();
+        }
+    }
+}
+   
--- UserRegistration/UserRegistration/Program.cs
+++ UserRegistration/UserRegistration/Program.cs
@@ -8,9 +8,13 @@ namespace UserRegistration
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("WelCome To User Registration Program");
+         static void Main(string[] args)
+            {
+                Console.WriteLine("Welcome to Regular Expression Program \n");
+                Console.WriteLine("Write your name using 1st character should be Capital ");
+                Console.Write("\n Enter First Name : ");
+                string name = Console.ReadLine();
+                Patterns.FirstName(name);
+            }
         }
     }
-}
