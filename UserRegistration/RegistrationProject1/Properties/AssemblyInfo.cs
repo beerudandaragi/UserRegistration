88fc1fa984794e10d7b459d60a25730925fa6327
beerappa <beerappadandaragi@gmail.com>
2021-12-05 22:48:17 +0530

    RegeMsUnitTest

---
+++ UserRegistration/RegistrationProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RegistrationProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RegistrationProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("531f311c-3144-4097-9b24-9f2fbac4194c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UserRegistration/RegistrationProject1/UnitTest1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RegEx;
+
+namespace RegistrationProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void FirstName()
+        {
+            string msg = " ";
+            string expected = "Valid";
+
+            Patterns p = new Patterns(msg);
+
+            string actual = p.FirstName("Valid");
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void LastName()
+        {
+            string msg = " ";
+            string expected = "Valid";
+
+            Patterns p = new Patterns(msg);
+
+            string actual = p.LastName("Valid");
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Email()
+        {
+            string msg = " ";
+            string expected = "Invalid";
+
+            Patterns p = new Patterns(msg);
+
+            string actual = p.Email("Invalid");
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void MobileNumber()
+        {
+            string msg = " ";
+            string expected = "Invalid";
+
+            Patterns p = new Patterns(msg);
+
+            string actual = p.MobileNumber("Invalid");
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Password()
+        {
+            string msg = " ";
+            string expected = "Invalid";
+
+            Patterns p = new Patterns(msg);
+
+            string actual = p.Password("Invalid");
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
